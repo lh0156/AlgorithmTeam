8d933724b4767fd36d2b675e5fd4d5a346651da0
hyein8290 <hyein8290@gmail.com>
2022-02-07 22:52:59 +0900

    포기하는 법을 배웠다

---
+++ Algorithm-Team/src/com/backjoon/b200/q1406/Hyein.java
@@ -0,0 +1,73 @@
+package com.backjoon.b200.q1406;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Stack;
+
+public class Hyein {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		String input = reader.readLine();
+		int M = Integer.parseInt(reader.readLine());
+		
+		Stack<Character> lStack = new Stack<Character>();
+		Stack<Character> rStack = new Stack<Character>();
+		
+		for(int i = 0; i < input.length(); i++) {
+			
+			lStack.push(input.charAt(i));
+		}
+				
+		for(int i = 0; i < M; i++) {
+			
+			String command = reader.readLine();
+			
+			switch (command.charAt(0)) {
+			
+			case 'L' :
+				if(!lStack.empty()) {
+					rStack.push(lStack.pop());					
+				}
+				break;
+				
+			case 'D' :
+				if(!rStack.empty()) {
+					rStack.push(lStack.pop());
+				}
+				break;
+				
+			case 'B' :
+				if(!lStack.empty()) {
+					lStack.pop();
+				}
+				break;
+				
+			case 'P' :
+				lStack.push(command.charAt(2));
+				break;
+
+			default:
+				break;
+			}
+			
+		}
+		
+		while(!lStack.empty()) {
+			rStack.push(lStack.pop());
+		}
+		
+		while(!lStack.empty()) {
+			writer.write(rStack.pop());
+		}
+		writer.flush();
+		writer.close();
+		
+	}
+}
