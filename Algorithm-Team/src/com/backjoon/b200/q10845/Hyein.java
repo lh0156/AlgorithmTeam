47f4e9eb2e6c8f9dc7230e57a1eee8af76f42598
hyein8290 <90780701+hyein8290@users.noreply.github.com>
2022-02-08 22:29:48 +0900

    Update Comment

--- Algorithm-Team/src/com/backjoon/b200/q10845/Hyein.java
+++ Algorithm-Team/src/com/backjoon/b200/q10845/Hyein.java
@@ -13,22 +13,22 @@ public class Hyein {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		
 		/*
-		 * 1. BufferedReader 생성 
-		 * 2. 결과값 저장할 StringBuilder result 생성
-		 * 2. N = 입력 받을 명령어 수
-		 * 3. N길이의 int[] queue 생성
-		 * 4. int frontIndex = 0, backIndex = -1
-		 * 5. N번 동안 for문
+		 * 1. int[] queue, int frontIndex = 0, backIndex = -1 전역변수로 선언
+		 * 2. BufferedReader 생성 
+		 * 3. 결과값 저장할 StringBuilder result 생성
+		 * 4. N = 입력 받을 명령어 수
+		 * 5. queue 사이즈 N으로 초기화
+		 * 6. N번 동안 for문
 		 * 		> command 배열에 명령어를 공백으로 split한 후 저장
 		 * 		> command[0]으로 switch문 분기
 		 * 			> push : backIndex++; queue[backIndex] = command[1];
-		 * 			> pop : return queue[frontIndex++] 
+		 * 			> pop : 비어있지 않으면 return queue[frontIndex++] 
 		 * 			> size : return backIndex - frontIndex + 1
 		 * 			> empty : if(frontIndex > backIndex) 1 else 0
 		 * 			> front : 비어있지 않으면 return queue[frontIndex];
 		 * 			> back : 비어있지 않으면 return queue[backIndex];
 		 * 		> result에 반환값 append
-		 * 6. result 출력
+		 * 7. result 출력
 		 */
 		
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
