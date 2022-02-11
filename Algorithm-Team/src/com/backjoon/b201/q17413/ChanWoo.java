537be91daa2ea3e15d08d4a16003c171a516e725
chanwoo lee <lcw7407@naver.com>
2022-02-11 23:47:28 +0900

    feat : 17413 upload

--- Algorithm-Team/src/com/backjoon/b201/q17413/ChanWoo.java
+++ Algorithm-Team/src/com/backjoon/b201/q17413/ChanWoo.java
@@ -1,1 +1,52 @@
-d
+package com.backjoon.b201.q17413;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class ChanWoo {
+	public static void main(String[] args) throws Exception {
+
+		BufferedReader rd = new BufferedReader(new InputStreamReader(System.in));
+
+		String str = rd.readLine();
+		
+		int check = 0;
+		String q = "";
+		for (int i = 0; i < str.length(); i++) {
+			if (str.substring(i, i + 1).equals("<")) {
+				if (q.length() != 0) {
+					for (int j = q.length() - 1; j >= 0; j--) {
+						System.out.print(q.charAt(j));
+					}
+					q = "";
+				}
+				check = 1;
+				System.out.print(str.substring(i, i + 1));
+			} else if (str.substring(i, i + 1).equals(">")) {
+				check = 0;
+				System.out.print(str.substring(i, i + 1));
+			} else if (check == 0) {
+				if (str.charAt(i) == ' ') {
+					if (q.length() != 0) {
+						for (int j = q.length() - 1; j >= 0; j--) {
+							System.out.print(q.charAt(j));
+						}
+						q = "";
+					}
+					System.out.print(" ");
+				} else if (i == str.length() - 1) {
+					if (q.length() != 0) {
+						q += str.charAt(i);
+						for (int j = q.length() - 1; j >= 0; j--) {
+							System.out.print(q.charAt(j));
+						}
+					}
+				} else {
+					q += str.charAt(i);
+				}
+			} else if (check == 1) {
+				System.out.print(str.charAt(i));
+			}
+		}
+	}
+}
