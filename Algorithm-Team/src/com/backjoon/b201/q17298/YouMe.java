6955ef8d72d502cb5ffe24c31f6eae34c0009511
umingming <u_0112@naver.com>
2022-02-13 14:05:53 +0900

    Update output method to StringBuilder

--- Algorithm-Team/src/com/backjoon/b201/q17298/YouMe.java
+++ Algorithm-Team/src/com/backjoon/b201/q17298/YouMe.java
@@ -1,7 +1,6 @@
 package com.backjoon.b201.q17298;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 
@@ -26,6 +25,8 @@ public class YouMe {
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder builder = new StringBuilder();
+		
 		int size = Integer.parseInt(reader.readLine());
 		String[] sequence = reader.readLine().split(" ");
 		
@@ -40,15 +41,17 @@ public class YouMe {
 			
 			for(int j=i+1; j<size; j++) {
 				if(list.get(i) < list.get(j)) {
-					System.out.print(list.get(j) + " ");
+					builder.append(list.get(j) + " ");
 					flag = true;
 					break;
 				}
 			}
 			
 			if(!flag) {
-				System.out.print("-1 ");
+				builder.append("-1 ");
 			}
 		}
+		
+		System.out.println(builder);
 	}
 }
