36180d2a4562cd32b68d02f4f57084d9a699a9ec
Hwang-97 <qqq4472@gmail.com>
2022-02-13 15:16:28 +0900

    backjoon_b201_q17298_HyunWoo

--- Algorithm-Team/src/com/backjoon/b201/q17298/HyunWoo.java
+++ Algorithm-Team/src/com/backjoon/b201/q17298/HyunWoo.java
@@ -1,51 +1,34 @@
 package com.backjoon.b201.q17298;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+
 
 public class HyunWoo {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-		List<Integer> list = new ArrayList<Integer>();
-		List<Integer> result = new ArrayList<Integer>();
 		StringBuilder sb = new StringBuilder();
-		int size = 0;
-		String[] arr = null;
-		try {
-			size = Integer.parseInt(reader.readLine());
-			arr = reader.readLine().split(" ");
-		} catch (NumberFormatException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		int size = Integer.parseInt(reader.readLine());
+		String[] arr = reader.readLine().split(" ");
 		
 		for(int i = 0 ; i < size ; i++) {
-			list.add(Integer.parseInt(arr[i]));
-		}
-		
-		for(int i = 0 ; i < size ; i++) {
-			int max = 0;
-			int nge=list.get(i);
-			for(int j=i; j<size ; j++) {
-				if(nge < list.get(j)) {
-					max = list.get(j);
-					result.add(list.get(j));
-					break;
-				}
-				if(j == size-1 && max==0) {
-					result.add(-1);
-					break;
+			int nge=Integer.parseInt(arr[i]);
+			if(i == size-1) {
+				sb.append(-1);
+				break;
+			}else {
+				for(int j=i; j<size ; j++) {
+					if(nge < Integer.parseInt(arr[j])) {
+						sb.append(Integer.parseInt(arr[j])).append(" ");
+						break;
+					}else if(j == size-1) {
+						sb.append(-1).append(" ");
+						break;
+					}
 				}
 			}
 		}
-		result.forEach(s->sb.append(s+" "));
-		System.out.println(sb.toString().substring(0,sb.length()-1));
-
+		System.out.println(sb.toString());
 	}
-
 }
