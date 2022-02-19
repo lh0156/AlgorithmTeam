2cb2722d79a5471fe5ebe462c4c61b61d4325303
umingming <u_0112@naver.com>
2022-02-20 01:35:47 +0900

    Solve 2743 using length()

---
+++ Algorithm-Team/src/com/backjoon/b203/q2743/YouMe.java
@@ -0,0 +1,20 @@
+package com.backjoon.b203.q2743;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class YouMe {
+	/*
+		단어의 길이를 출력하는 프로그램
+		
+		설계
+		1. BufferedReader 생성
+		2. 입력 값을 input 변수에 초기화함.
+		3. input의 길이를 출력
+	 */
+	public static void main(String[] args) throws Exception {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		String input = reader.readLine();
+		System.out.println(input.length());
+	}
+}
