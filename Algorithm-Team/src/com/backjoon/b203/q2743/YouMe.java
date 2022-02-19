cdecc74cb302fe0ad2ab182fb8abe60be8417971
umingming <u_0112@naver.com>
2022-02-20 01:36:29 +0900

    Remove input variable

--- Algorithm-Team/src/com/backjoon/b203/q2743/YouMe.java
+++ Algorithm-Team/src/com/backjoon/b203/q2743/YouMe.java
@@ -14,7 +14,6 @@ public class YouMe {
 	 */
 	public static void main(String[] args) throws Exception {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-		String input = reader.readLine();
-		System.out.println(input.length());
+		System.out.println(reader.readLine().length());
 	}
 }
