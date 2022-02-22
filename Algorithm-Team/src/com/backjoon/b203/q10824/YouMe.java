5bdedd1a0bb7c8c12c066335a8631ba9d786de40
umingming <u_0112@naver.com>
2022-02-22 15:11:11 +0900

    Solve 10824 using split()

---
+++ Algorithm-Team/src/com/backjoon/b203/q10824/YouMe.java
@@ -0,0 +1,25 @@
+package com.backjoon.b203.q10824;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class YouMe {
+	/*
+		AB + CD 구하는 프로그램
+		
+		설계> 
+		1. BufferedReader 선언
+		2. 입력 값을 쪼개 num 배열에 초기화
+		3. AB와 BC를 형변환 후 더해 sum 변수에 초기화함.
+		4. sum 출력
+	 */
+	public static void main(String[] args) throws Exception {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		String[] num = reader.readLine().split(" ");
+		
+		int sum = Integer.parseInt(num[0] + num[1])
+					+ Integer.parseInt(num[2] + num[3]);
+		
+		System.out.println(sum);
+	}
+}
