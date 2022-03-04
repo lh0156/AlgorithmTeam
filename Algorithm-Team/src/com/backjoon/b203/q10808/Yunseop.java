89ffbd1d1a9e136b3ace058ede82b2f36151e8ac
YunSeop <62834176+lh0156@users.noreply.github.com>
2022-03-04 13:03:00 +0900

    add class

---
+++ Algorithm-Team/src/com/backjoon/b203/q10808/Yunseop.java
@@ -0,0 +1,11 @@
+package com.backjoon.b203.q10808;
+
+public class Yunseop {
+
+	public static void main(String[] args) {
+		
+		
+		
+	}
+	
+}
---
+++ Algorithm-Team/src/com/backjoon/b300/q6588/Yunseop.java
@@ -0,0 +1,12 @@
+package com.backjoon.b300.q6588;
+
+public class Yunseop {
+
+	public static void main(String[] args) {
+		
+		
+		
+		
+	}
+	
+}
