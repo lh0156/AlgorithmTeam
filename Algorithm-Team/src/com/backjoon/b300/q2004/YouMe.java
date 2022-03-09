8470af1beb25c23b3ea43d98fe8309a434609135
umingming <u_0112@naver.com>
2022-03-09 19:33:31 +0900

    Solve 2004 Using BufferedReader

--- Algorithm-Team/src/com/backjoon/b300/q2004/YouMe.java
+++ Algorithm-Team/src/com/backjoon/b300/q2004/YouMe.java
@@ -11,16 +11,15 @@ public class YouMe {
 		설계>
 		1. BufferedReader 입력 받음.
 		2. long 변수로 선언한 n과 m을 입력 값으로 초기화함.
-		3. cntFive 변수에 getFivePower(n) - getFivePower(n-m) - getFivePower(m) 메소드 호출 후 저장함.
-		4. getFivePower
+		3. cntFive 변수에 getNthRootOf(n, 5) - getNthRootOf(n-m, 5) - getNthRootOf(m, 5) 메소드 호출 후 저장함.
+		4. cntTwo 변수에 getNthRootOf(n, 2) - getNthRootOf(n-m, 2) - getNthRootOf(m, 2) 메소드 호출 후 저장함.
+		5. getNthRootOf 메소드;
 			> cnt 변수 선언 후 0으로 초기화함.
-			> while문 num이 5이상일 경우를 조건으로 사용
-				> cnt 변수에 num을 5로 나눈 값 더함.
-				> num을 5로 나눔.
+			> while문 input이 num 이상일 경우를 조건으로 사용
+				> cnt 변수에 input을 num으로 나눈 값 더함.
+				> input을 num으로 나눔.
 			> cnt 리턴함.
-		5. cntTwo 변수에 getTwoPower(n) - getTwoPower(n-m) - getTwoPower(m) 메소드 호출 후 저장함.
-		6. getTwoPower; getFivePower의 5를 2로 변경한 메소드임.
-		7. cntFive와 cntTwo 중 작은 값 출력함.
+		6. cntFive와 cntTwo 중 작은 값 출력함.
 	 */
 	
 	public static void main(String[] args) throws Exception {
@@ -31,28 +30,18 @@ public class YouMe {
 		long n = Long.parseLong(input[0]);
 		long m = Long.parseLong(input[1]);
 		
-		long cntFive = getFivePower(n) - getFivePower(n-m) - getFivePower(m);
-		long cntTwo = getTwoPower(n) - getTwoPower(n-m) - getTwoPower(m);
+		long cntFive = getNthRootOf(n, 5) - getNthRootOf(n-m, 5) - getNthRootOf(m, 5);
+		long cntTwo = getNthRootOf(n, 2) - getNthRootOf(n-m, 2) - getNthRootOf(m, 2);
 
 		System.out.println(Math.min(cntFive, cntTwo));
 	}
 
-	private static long getTwoPower(long num) {
+	private static long getNthRootOf(long input, int num) {
 		int cnt = 0;
 		
-		while(num >= 2) {
-			cnt += num / 2;
-			num /= 2;
-		}
-		return cnt;
-	}
-
-	private static long getFivePower(long num) {
-		int cnt = 0;
-		
-		while(num >= 5) {
-			cnt += num / 5;
-			num /= 5;
+		while(input >= num) {
+			cnt += input / num;
+			input /= num;
 		}
 		return cnt;
 	}
