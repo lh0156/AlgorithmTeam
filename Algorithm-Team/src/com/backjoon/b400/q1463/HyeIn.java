a79c8c36fdd2d8a0f5eae4f065ae9f04a868ce06
hyein8290 <hyein8290@gmail.com>
2022-03-15 23:30:53 +0900

    Solve 1463

--- Algorithm-Team/src/com/backjoon/b400/q1463/HyeIn.java
+++ Algorithm-Team/src/com/backjoon/b400/q1463/HyeIn.java
@@ -1,5 +1,7 @@
 package com.backjoon.b400.q1463;
 
+import java.util.Scanner;
+
 public class HyeIn {
 
 	public static void main(String[] args) {
@@ -7,5 +9,24 @@ public class HyeIn {
 		/*
 		 * 
 		 */
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int N = scan.nextInt();
+		int[] dp = new int[N + 1];
+		
+		dp[0] = dp[1] = 0;
+		
+		for(int i=2; i<=N; i++) {
+			dp[i] = dp[i-1] + 1;
+			if(i % 2 == 0) {
+				dp[i] = Math.min(dp[i], dp[i/2] + 1);
+			}
+			if(i % 3 == 0) {				
+				dp[i] = Math.min(dp[i], dp[i/3] + 1);
+			}
+		}
+		
+		System.out.println(dp[N]);
 	}
 }
