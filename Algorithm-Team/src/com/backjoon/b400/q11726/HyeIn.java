ea9a38fd28cdfafeec6f66532e30023122371c1c
hyein8290 <hyein8290@gmail.com>
2022-03-16 23:55:52 +0900

    commit

---
+++ Algorithm-Team/src/com/backjoon/b400/q11726/HyeIn.java
@@ -0,0 +1,14 @@
+package com.backjoon.b400.q11726;
+
+public class HyeIn {
+
+	public static void main(String[] args) {
+		
+		/*
+		 * 2 x 1 = 2 x 1
+		 * 2 x 2 = 2 x 1 + 2 x 1
+		 * 		 = 1 x 2 + 1 x 2
+
+		 */
+	}
+}
